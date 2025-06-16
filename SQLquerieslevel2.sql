5b3768cccfdf79c17d5d6b5b15e8742f0e53609d
Eklavya-made <pandeyvivek1079@gmail.com>
2025-06-16 10:53:08 +0530

    Update SQLquerieslevel2.sql

--- SQLquerieslevel2.sql
+++ SQLquerieslevel2.sql
@@ -1,4 +1,4 @@
--- Task 13: Identify Members with Overdue Books
+-- Identify Members with Overdue Books
 -- Write a query to identify members who have overdue books (assume a 30-day return period). 
 -- Display the member's_id, member's name, book title, issue date, and days overdue.
 
@@ -32,7 +32,7 @@ WHERE
 ORDER BY 1
 
 
--- Task 14: Update Book Status on Return
+-- Update Book Status on Return
 -- Write a query to update the status of books in the books table to "Yes" 
 -- when they are returned (based on entries in the return_status table).
 
@@ -91,7 +91,7 @@ CALL add_return_records('RS138', 'IS135');
 -- calling function 
 CALL add_return_records('RS148', 'IS140', 'Good');
 
--- Task 15: Branch Performance Report
+-- Branch Performance Report
 -- Create a query that generates a performance report for each branch, 
 -- showing the number of books issued, the number of books returned, 
 -- and the total revenue generated from book rentals.
@@ -123,7 +123,7 @@ SELECT * FROM branch_reports;
 
 
 
--- Task 16: CTAS: Create a Table of Active Members
+-- CTAS: Create a Table of Active Members
 -- Use the CREATE TABLE AS (CTAS) statement to create a new table active_members containing members who have issued 
 -- at least one book in the last 2 months.
 
@@ -157,7 +157,7 @@ FROM issued_status
 ORDER BY issued_date DESC;
 
 
--- Task 17: Find Employees with the Most Book Issues Processed
+-- Find Employees with the Most Book Issues Processed
 -- Write a query to find the top 3 employees who have processed the most book issues. 
 -- Display the employee name, number of books processed, and their branch.
 
@@ -176,7 +176,7 @@ GROUP BY 1, 2
 
 
 
--- Task 19: Stored Procedure Objective: Create a stored procedure to manage the status of books in a library system. 
+-- Stored Procedure Objective: Create a stored procedure to manage the status of books in a library system. 
 -- Description: Write a stored procedure that updates the status of a book in the library based on its issuance. 
 -- The procedure should function as follows: The stored procedure should take the book_id as an input parameter. 
 -- The procedure should first check if the book is available (status = 'yes'). 
